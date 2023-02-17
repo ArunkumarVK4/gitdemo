595f5c14756e300d668b5ef7327dbedec21b6f8c
ArunkumarVK4 <vkarunkumar446@gmail.com>
2023-02-17 20:35:02 +0530

    test file upload

---
+++ Resume.js
@@ -0,0 +1,8 @@
+var resume = {
+    "name" : "Arunkumar VK",
+    "mobile number" : "+91 8610216170",
+    "email id" : "vkarunkumar466@gmail.com",
+    "qualification" : "B.E-Mechanical Engineering",
+    "college name" : "Ramco institute of technology",
+    "skill" : ["HTML", "CSS", "JAVASCRIPT","REACT","sQL"]
+}
